f8dab1411393fdab46592edef137d5f7686f99ca
Deborah Novais <61918962+deborahnovais@users.noreply.github.com>
2020-12-20 18:31:55 -0300

    Concluído

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ValidaçãoSenha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Validação de senha");
+            Console.WriteLine("-----------------------");
+
+            Console.WriteLine("Digite o seu nome:");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Digite a senha:");
+            string senha = Console.ReadLine();
+
+            while(nome == senha) {
+                Console.WriteLine("Senha inválida. Digite uma senha que não contenha o seu nome.");
+                senha = Console.ReadLine();
+            }  
+
+            Console.WriteLine("Senha cadastrada com sucesso.");     
+        }
+    }
+}
